4198186e1a3eff2c659e16a00aa75317acb9ab79
Mano <Koutouzosm@gmail.com>
2019-06-04 14:07:31 -0400

    firebase

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -2,8 +2,13 @@ import app from 'firebase/app'
 import 'firebase/database'
 
 const config = {
-
-}
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
+};
 
 class Firebase {
     constructor() {
@@ -12,12 +17,12 @@ class Firebase {
     }
 
 
-// DEFINE FIREBASE METHODS
-// message is an object
+    // DEFINE FIREBASE METHODS
+    // message is an object
 
-send = message => this.db.ref('chat').push(message)
+    send = message => this.db.ref('chat').push(message)
 
-chat = () => this.db.ref('chat')
+    chat = () => this.db.ref('chat')
 
 
 }
